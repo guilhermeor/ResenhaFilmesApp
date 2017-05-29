e6e070514a967ddf3961ec1b317b9ecc69bb2b97
Guilherme Oliveira da Rosa <guilhermeor.rs@gmail.com>
2017-05-28 21:26:17 -0300

    Added class string extensions with extension methods to string.

---
+++ ResenhaFilmesApp/ResenhaFilmesApp/ResenhaFilmesApp/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ResenhaFilmesApp.ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static bool IsNotNullOrWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str).Equals(false);
+        }
+    }
+}

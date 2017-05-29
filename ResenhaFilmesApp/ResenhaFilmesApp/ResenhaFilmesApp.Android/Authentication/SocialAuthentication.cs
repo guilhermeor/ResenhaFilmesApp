fffb61b23e71ba858d76ae60b8ca564f1069874d
Guilherme Oliveira da Rosa <guilhermeor.rs@gmail.com>
2017-05-28 21:59:20 -0300

    Added class to login method using authentication

---
+++ ResenhaFilmesApp/ResenhaFilmesApp/ResenhaFilmesApp.Android/Authentication/SocialAuthentication.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.WindowsAzure.MobileServices;
+using System.Threading.Tasks;
+using ResenhaFilmesApp.Droid.Authentication;
+using Xamarin.Forms;
+using ResenhaFilmesApp.Authentication;
+using ResenhaFilmesApp.Helpers;
+
+[assembly: Dependency(typeof(SocialAuthentication))]
+namespace ResenhaFilmesApp.Droid.Authentication
+{
+    public class SocialAuthentication : IAuthentication
+    {
+        public async Task<MobileServiceUser> LoginAsync(MobileServiceClient client, MobileServiceAuthenticationProvider provider, IDictionary<string, string> parameters = null)
+        {
+            try
+            {
+                var user = await client.LoginAsync(Forms.Context, provider);
+
+                Settings.AuthToken = user?.MobileServiceAuthenticationToken ?? string.Empty;
+                Settings.UserId = user?.UserId;
+
+                return user;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}

b99577b097f7ab5e40b305294849a482f97b3d8a
Guilherme Oliveira da Rosa <guilhermeor.rs@gmail.com>
2017-05-28 22:16:55 -0300

    Created methods to azure service  at authentication.

---
+++ ResenhaFilmesApp/ResenhaFilmesApp/ResenhaFilmesApp/Services/AzureService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using ResenhaFilmesApp.Authentication;
+using ResenhaFilmesApp.ExtensionMethods;
+using ResenhaFilmesApp.Helpers;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ResenhaFilmesApp.Services
+{
+    public class AzureService
+    {
+        static readonly string AppUrl = "https://gsociallogin.azurewebsites.net";
+
+        public MobileServiceClient Client { get; set; } = null;
+
+        public static bool UseAuth { get; set; } = false;
+
+        public void Initialize()
+        {
+            Client = new MobileServiceClient(AppUrl);
+
+            if (Settings.AuthToken.IsNotNullOrWhiteSpace() && Settings.UserId.IsNotNullOrWhiteSpace())
+            {
+                Client.CurrentUser = new MobileServiceUser(Settings.UserId)
+                {
+                    MobileServiceAuthenticationToken = Settings.AuthToken
+                };
+            }
+        }
+        public async Task<bool> LoginAsync()
+        {
+            Initialize();
+
+            var auth = DependencyService.Get<IAuthentication>();
+            var user = await auth.LoginAsync(Client, MobileServiceAuthenticationProvider.Facebook);
+
+            if (user == null)
+            {
+                Settings.AuthToken = string.Empty;
+                Settings.UserId = string.Empty;
+
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await App.Current.MainPage.DisplayAlert("Ops!", "Não foi possível efetuar o login", "OK");
+                });
+
+                return false;
+            }
+            else
+            {
+                Settings.AuthToken = user.MobileServiceAuthenticationToken;
+                Settings.UserId = user.UserId;
+            }
+
+            return true;
+        }
+    }
+}
